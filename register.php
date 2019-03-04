435e70e96c8a8d356ab19411fb7e5c26e19a715b
unezanakhwa <45363640+unezanakhwa@users.noreply.github.com>
2019-03-04 23:44:54 +0530

    Add files via upload

---
+++ register.php
@@ -0,0 +1,33 @@
+<!Doctype html>
+<html>
+<head>
+<title>Registration Form</title>
+</head>
+<body>
+<h1>Registration Form</h1>
+<form method="post" action="db.php">
+<table>
+<tr>
+<td>Name:</td>
+<td><input type="text" name="name" class="textInput"></td>
+</tr>
+<tr>
+<td>Phone Number:</td>
+<td><input type="text" name="phone" class="textInput"></td>
+</tr>
+<tr>
+<td>Email Address:</td>
+<td><input type="text" name="email" class="textInput"></td>
+</tr>
+<tr>
+<td>Username:</td>
+<td><input type="text" name="name" class="textInput"></td>
+</tr>
+<tr>
+<td>Password:</td>
+<td><input type="password" name="pass" class="textInput"></td>
+</tr>
+<td><input type="submit" name="submit_button" value="Submit"></td>
+</table>
+</body>
+</html>
